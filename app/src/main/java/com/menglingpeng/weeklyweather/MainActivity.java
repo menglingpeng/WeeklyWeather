08091ed16a8242a4654ea91e656b0db0b3c81c51
menglingpeng <menglingpeng@live.com>
2018-01-13 12:06:51 +0800

    feat: add init NavigationView

--- app/src/main/java/com/menglingpeng/weeklyweather/MainActivity.java
+++ app/src/main/java/com/menglingpeng/weeklyweather/MainActivity.java
@@ -1,28 +1,33 @@
 package com.menglingpeng.weeklyweather;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
-import android.location.Location;
-import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.menglingpeng.weeklyweather.utils.Constants;
 import com.menglingpeng.weeklyweather.utils.LocationUtils;
 import com.menglingpeng.weeklyweather.utils.RequestPermissionUtil;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
     private NavigationView navigationView;
+    private ImageView navHeaderBackgroudIv;
+    private RelativeLayout navHeaderRl;
     private Context context = getApplicationContext();
 
     @Override
@@ -46,6 +51,7 @@ public class MainActivity extends BaseActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
         toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,4 +64,45 @@ public class MainActivity extends BaseActivity {
         });
     }
 
+    @SuppressLint("RestrictedApi")
+    private void initNavigationView(){
+        View headerView = navigationView.getHeaderView(0);
+        navHeaderRl = (RelativeLayout)headerView.findViewById(R.id.bav_header_rl);
+        navHeaderBackgroudIv = (ImageView)headerView.findViewById(R.id.nav_header_backgroud_iv);
+        //设置关闭和打开特效
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.nav_drawer_open ,R.string.nav_drawer_close);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        toggle.syncState();
+        drawerLayout.addDrawerListener(toggle);
+        navigationView.inflateMenu(R.menu.nav_content_menu);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            navigationView.setCheckedItem(R.id.nav_home_menu);
+        }else {
+            navigationView.getMenu().findItem(0).setCheckable(true);
+        }
+        //修改NavigationView的menu选中的icon和text颜色，默认是跟随主题色
+        ColorStateList csl = getResources().getColorStateList(R.color.navigationview_menu_item_color);
+        navigationView.setItemIconTintList(csl);
+        navigationView.setItemTextColor(csl);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_home_menu:
+                break;
+            case R.id.nav_share_menu:
+                break;
+            case R.id.nav_settings_menu:
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
 }
