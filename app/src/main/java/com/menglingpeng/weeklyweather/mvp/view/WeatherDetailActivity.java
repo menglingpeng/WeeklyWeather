02c3aa155111a89e1bf37a028ebdc783aef7abed
menglingpeng <menglingpeng@live.com>
2018-01-30 20:36:31 +0800

    feat: add custom tablayout for WeatherDetailActivity

--- app/src/main/java/com/menglingpeng/weeklyweather/mvp/view/WeatherDetailActivity.java
+++ app/src/main/java/com/menglingpeng/weeklyweather/mvp/view/WeatherDetailActivity.java
@@ -5,14 +5,18 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.menglingpeng.weeklyweather.BaseActivity;
 import com.menglingpeng.weeklyweather.R;
 import com.menglingpeng.weeklyweather.mvp.adapter.WeatherDetailTabPagerAdapter;
 import com.menglingpeng.weeklyweather.utils.Constants;
+
+import java.util.ArrayList;
 
 public class WeatherDetailActivity extends BaseActivity {
 
@@ -22,6 +26,10 @@ public class WeatherDetailActivity extends BaseActivity {
     private int currentItemId;
     private String currentCity;
     private WeatherDetailTabPagerAdapter adapter;
+    private ArrayList<String> topTitles;
+    private ArrayList<String> bottomTitles;
+    private ArrayList<WeatherDetailFragment> fragments;
+
     @Override
     protected void initLayoutId() {
         layoutId = R.layout.activity_weather_detail;
@@ -32,6 +40,8 @@ public class WeatherDetailActivity extends BaseActivity {
         super.initViews();
         currentItemId = Integer.valueOf(getIntent().getStringExtra(Constants.CURRENT_DAY_POSITION));
         toolbar = (Toolbar)findViewById(R.id.detail_tb);
+        topTitles = new ArrayList<>();
+        bottomTitles = new ArrayList<>();
         toolbar.setTitle(currentCity);
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
@@ -61,6 +71,8 @@ public class WeatherDetailActivity extends BaseActivity {
         adapter = new WeatherDetailTabPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+        setTabView();
+        viewPager.setCurrentItem(currentItemId);
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -77,5 +89,20 @@ public class WeatherDetailActivity extends BaseActivity {
 
             }
         });
+    }
+
+    private void setTabView(){
+        for (int i = 0; i < topTitles.size(); i++){
+            tabLayout.getTabAt(i).setCustomView(getTabView(i));
+        }
+    }
+
+    private View getTabView(int position){
+        View tabView = LayoutInflater.from(this).inflate(R.layout.tablayout_weather_detail_tab_item, null);
+        TextView topTitleTv = (TextView)tabView.findViewById(R.id.weather_detail_tl_tab_top_tv);
+        TextView bottomTitleTv = (TextView)tabView.findViewById(R.id.weather_detail_tl_tab_bottom_tv);
+        topTitleTv.setText(topTitles.get(position));
+        bottomTitleTv.setText(bottomTitles.get(position));
+        return tabView;
     }
 }
