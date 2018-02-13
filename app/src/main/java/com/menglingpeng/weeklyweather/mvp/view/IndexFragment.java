8ef0dd5619eaacc9b89cb8fa2cdd20512ebe7998
menglingpeng <menglingpeng@live.com>
2018-02-13 17:10:25 +0800

    feat: add layout of IndexFragment

--- app/src/main/java/com/menglingpeng/weeklyweather/mvp/view/IndexFragment.java
+++ app/src/main/java/com/menglingpeng/weeklyweather/mvp/view/IndexFragment.java
@@ -1,10 +1,30 @@
 package com.menglingpeng.weeklyweather.mvp.view;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
+
+import com.menglingpeng.weeklyweather.BaseFragment;
+import com.menglingpeng.weeklyweather.R;
 
 /**
  * Created by mengdroid on 2018/2/12.
  */
 
-public class IndexFragment extends Fragment {
+public class IndexFragment extends BaseFragment {
+
+    private Context context;
+    @Override
+    protected void initLayoutId() {
+        layoutId = R.layout.fragment_index;
+    }
+
+    @Override
+    protected void initView() {
+    context = getActivity().getApplicationContext();
+    }
+
+    @Override
+    protected void initData() {
+
+    }
 }
