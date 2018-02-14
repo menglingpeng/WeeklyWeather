24b5d0b8a4254a6d2f659253eb98f2f8c04bb2b3
menglingpeng <menglingpeng@live.com>
2018-02-14 19:26:06 +0800

    feat: add layout of ColdIndexActivity

---
+++ app/src/main/java/com/menglingpeng/weeklyweather/mvp/view/ColdIndexActivity.java
@@ -0,0 +1,53 @@
+package com.menglingpeng.weeklyweather.mvp.view;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.menglingpeng.weeklyweather.BaseActivity;
+import com.menglingpeng.weeklyweather.R;
+
+public class ColdIndexActivity extends BaseActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void initLayoutId() {
+        layoutId = R.layout.activity_cold_index;
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        toolbar = (Toolbar)findViewById(R.id.cold_index_tb);
+        tabLayout = (TabLayout)findViewById(R.id.cold_index_tl);
+        viewPager = (ViewPager)findViewById(R.id.cold_index_vp);
+        toolbar.setTitle(R.string.cold_index);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.acticity_index_toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
