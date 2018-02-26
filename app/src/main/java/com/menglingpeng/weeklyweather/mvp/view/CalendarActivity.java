10d8ab03702180fb4c35eb9f78c37eeef4cfa342
menglingpeng <menglingpeng@live.com>
2018-02-26 20:41:50 +0800

    feat: add layout of CalendarActivity

---
+++ app/src/main/java/com/menglingpeng/weeklyweather/mvp/view/CalendarActivity.java
@@ -0,0 +1,55 @@
+package com.menglingpeng.weeklyweather.mvp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import com.menglingpeng.weeklyweather.BaseActivity;
+import com.menglingpeng.weeklyweather.R;
+
+public class CalendarActivity extends BaseActivity {
+
+    private Toolbar toolbar;
+    private TextView holidayTv;
+    private TextView titleTv;
+    private CalendarView calendarView;
+
+    @Override
+    protected void initLayoutId() {
+        layoutId = R.layout.activity_calendar;
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        toolbar = (Toolbar)findViewById(R.id.car_wash_index_tb);
+        holidayTv = (TextView)findViewById(R.id.activity_calendar_holiday_tv);
+        titleTv = (TextView)findViewById(R.id.activity_calendar_title_tv);
+        calendarView = (CalendarView)findViewById(R.id.activity_calendar_cv);
+        toolbar.setTitle(R.string.calendar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.acticity_index_toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
