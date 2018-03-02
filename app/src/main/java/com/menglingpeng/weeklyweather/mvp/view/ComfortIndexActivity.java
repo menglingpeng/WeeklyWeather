cbe0af6f9e98cd44dbfe7ebc78336c7f87a37c94
menglingpeng <menglingpeng@live.com>
2018-03-02 21:40:40 +0800

    feat: add layout of ComfortIndexActivity

---
+++ app/src/main/java/com/menglingpeng/weeklyweather/mvp/view/ComfortIndexActivity.java
@@ -0,0 +1,21 @@
+package com.menglingpeng.weeklyweather.mvp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.menglingpeng.weeklyweather.BaseActivity;
+import com.menglingpeng.weeklyweather.R;
+
+public class ComfortIndexActivity extends BaseActivity {
+
+
+    @Override
+    protected void initLayoutId() {
+        layoutId = R.layout.activity_comfort_index;
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+    }
+}
