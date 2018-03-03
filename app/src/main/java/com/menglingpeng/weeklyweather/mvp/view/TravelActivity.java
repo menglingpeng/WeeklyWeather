6dbe942cfb571eb110a2e3db4e4005d7be973a28
menglingpeng <menglingpeng@live.com>
2018-03-03 21:29:36 +0800

    feat: add layout of TravelIndexActivity

---
+++ app/src/main/java/com/menglingpeng/weeklyweather/mvp/view/TravelActivity.java
@@ -0,0 +1,21 @@
+package com.menglingpeng.weeklyweather.mvp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.menglingpeng.weeklyweather.BaseActivity;
+import com.menglingpeng.weeklyweather.R;
+
+public class TravelActivity extends BaseActivity {
+
+
+    @Override
+    protected void initLayoutId() {
+        layoutId = R.layout.activity_travel;
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+    }
+}
