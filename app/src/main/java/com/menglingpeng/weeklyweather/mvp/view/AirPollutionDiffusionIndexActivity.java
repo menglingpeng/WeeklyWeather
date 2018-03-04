5323e97069f65c6930855a8b4b5df0ea26399274
menglingpeng <menglingpeng@live.com>
2018-03-04 18:22:01 +0800

    feat: add layout of AirPollutionDiffusionIndexActivity

---
+++ app/src/main/java/com/menglingpeng/weeklyweather/mvp/view/AirPollutionDiffusionIndexActivity.java
@@ -0,0 +1,23 @@
+package com.menglingpeng.weeklyweather.mvp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.menglingpeng.weeklyweather.BaseActivity;
+import com.menglingpeng.weeklyweather.R;
+
+import javax.crypto.spec.RC2ParameterSpec;
+
+public class AirPollutionDiffusionIndexActivity extends BaseActivity {
+
+
+    @Override
+    protected void initLayoutId() {
+        layoutId = R.layout.activity_air_pollution_diffusion_index;
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+    }
+}
