c39dd8a1031b03f316c01dbe7bb663e3b0fe4698
menglingpeng <menglingpeng@live.com>
2018-03-05 21:24:38 +0800

    feat: add the function of init view for FluIndexActivity

--- app/src/main/java/com/menglingpeng/weeklyweather/mvp/view/FluIndexActivity.java
+++ app/src/main/java/com/menglingpeng/weeklyweather/mvp/view/FluIndexActivity.java
@@ -1,12 +1,34 @@
 package com.menglingpeng.weeklyweather.mvp.view;
 
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.menglingpeng.weeklyweather.BaseActivity;
 import com.menglingpeng.weeklyweather.R;
+import com.menglingpeng.weeklyweather.mvp.adapter.TabPagerFragmentAdapter;
+import com.menglingpeng.weeklyweather.mvp.bean.WeatherCollection;
+import com.menglingpeng.weeklyweather.utils.Constants;
+import com.menglingpeng.weeklyweather.utils.IndexActivityUtils;
+
+import java.util.ArrayList;
 
 public class FluIndexActivity extends BaseActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ArrayList<String> titles;
+    private ArrayList<IndexFragment> fragments;
+    private TabPagerFragmentAdapter adapter;
+    private Context context;
+    private WeatherCollection weatherCollection;
 
 
     @Override
@@ -17,5 +39,34 @@ public class FluIndexActivity extends BaseActivity {
     @Override
     protected void initViews() {
         super.initViews();
+        context = getApplicationContext();
+        weatherCollection = (WeatherCollection) getIntent().getSerializableExtra(Constants.WEATHER_COLLECTION);
+        toolbar = (Toolbar)findViewById(R.id.flu_index_tb);
+        tabLayout = (TabLayout)findViewById(R.id.flu_index_tl);
+        viewPager = (ViewPager)findViewById(R.id.flu_index_vp);
+        titles = new ArrayList<>();
+        fragments = new ArrayList<>();
+        toolbar.setTitle(R.string.flu_index);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        adapter = new TabPagerFragmentAdapter(getSupportFragmentManager());
+        IndexActivityUtils.initTablayout(context, tabLayout, viewPager, titles, fragments, adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.acticity_index_toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
