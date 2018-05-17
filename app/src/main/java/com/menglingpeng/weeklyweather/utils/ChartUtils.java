bae4d3b70494d374eb8dc628d9c05de18e873eef
menglingpeng <menglingpeng@live.com>
2018-05-17 20:06:19 +0800

    feat: add the bar chart for 24hours forecast

--- app/src/main/java/com/menglingpeng/weeklyweather/utils/ChartUtils.java
+++ app/src/main/java/com/menglingpeng/weeklyweather/utils/ChartUtils.java
@@ -4,17 +4,24 @@ import android.content.Context;
 import android.graphics.Color;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.MarkerView;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.ChartData;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.menglingpeng.weeklyweather.R;
 
 import java.util.ArrayList;
@@ -291,4 +298,68 @@ class CustomMarkerView extends MarkerView {
         // 提示框在坐标点上方显示
         return -getHeight();
     }
+
+    public static BarChart initBarChart(LineChart chart, List<Entry> values){
+        //创建条形数据对象
+        BarChart barChart = new BarChart(this);
+        setContentView(barChart);
+       //设置条形数据
+        barChart.setData(getBarData());
+       //设置描述
+        barChart.setDescription("");
+      //设置绘制bar的阴影
+        barChart.setDrawBarShadow(true);
+        //设置绘制的动画时间
+        barChart.animateXY(3000,3000);
+        return barChart;
+
+    }
+
+    public BarData getBarData() {
+        int maxX = 10;
+        //创建集合，存放每个柱子的数据
+        List<BarEntry> list = new ArrayList<>();
+        List<BarEntry> list2 = new ArrayList<>();
+        for (int i = 0; i < maxX; i++) {
+            //一个BarEntry就是一个柱子的数据对象
+            BarEntry barEntry = new BarEntry(i + 5, i);
+            list.add(barEntry);
+            BarEntry barEntry2 = new BarEntry(i + 3, i);
+            list2.add(barEntry2);
+        }
+        //创建BarDateSet对象，其实就是一组柱形数据
+        BarDataSet barSet = new BarDataSet(list, "Android");
+        BarDataSet barSet2 = new BarDataSet(list2, "iOS");
+        //设置柱形的颜色
+        barSet.setColor(Color.BLUE);
+        //设置是否显示柱子上面的数值
+        barSet.setDrawValues(false);
+        //设置柱子阴影颜色
+        barSet.setBarShadowColor(Color.GRAY);
+        //创建集合，存放所有组的柱形数据
+        List<IBarDataSet> dataSets = new ArrayList<>();
+        dataSets.add(barSet);
+        dataSets.add(barSet2);
+        BarData barData = new BarData(ChartData.generateXVals(0, maxX), dataSets);
+        return barData;
+    }
+
+    public static PieChart initBarChart(LineChart chart, List<Entry> values){
+        PieChart pieChart = new PieChart(this);
+        setContentView(pieChart);
+
+       //设置饼状图数据
+        pieChart.setData(getPieData());
+
+       //设置描述
+        pieChart.setDescription("");
+        pieChart.setDescriptionTextSize(20);
+
+        //设置中心说明文字
+        pieChart.setCenterText("中心说明文字");
+        pieChart.setCenterTextSize(20);
+        pieChart.setCenterTextColor(Color.RED);
+
+        pieChart.animateXY(3000,3000);
+
 }
